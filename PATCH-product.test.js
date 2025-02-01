55beec4862bfa0544cbaafcecbf7d476df9abc5b
Reza Vahlevi <reza.vahlevi@BLUEBIRDGROUP.COM>
2025-02-01 15:08:32 +0700

    new patch product

--- PATCH-product.test.js
+++ PATCH-product.test.js
@@ -54,4 +54,64 @@ describe("Update Products By Id", () => {
     expect(response.body).toHaveProperty("success");
     console.log(response.body)
   });
+
+  test("Create Product - Invalid endpoint (404)", async () => {
+    const response = await request(baseURL)
+      .patch(`/productss/${productId}`)
+      .send({
+        name: "invalid endpoint",
+        description: "invalid endpoint",
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: imageId,
+        is_location_offer: 1,
+        is_rental: 0,
+      })
+      .set("Content-Type", "application/json");
+
+    expect(response.status).toBe(404);
+    console.log("actual status: " + response.status + ", expected status 404");
+    console.log("response body:", response.body)
+  });
+
+  test("Create Product - Invalid method (405)", async () => {
+    const response = await request(baseURL)
+      .post(`/products/${productId}`)
+      .send({
+        name: "invalid method",
+        description: "invalid method",
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: imageId,
+        is_location_offer: 1,
+        is_rental: 0,
+      })
+      .set("Content-Type", "application/json");
+
+    expect(response.status).toBe(405);
+    console.log("actual status: " + response.status + ", expected status 405");
+    console.log("response body:", response.body)
+  });
+
+  test("Create Product - invalid server was not able to process (422)", async () => {
+    const response = await request(baseURL)
+      .patch(`/products/${productId}`)
+      .send({
+        name: "invalid server",
+        description: "invalid server",
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: imageId,
+        is_location_offer: 5,
+        is_rental: 6,
+      })
+      .set("Content-Type", "application/json");
+
+    expect(response.status).toBe(422);
+    console.log("actual status: " + response.status + ", expected status 422");
+    console.log("response body:", response.body)
+  });
 });
